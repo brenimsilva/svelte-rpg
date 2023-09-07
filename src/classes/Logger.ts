430ba3e35b50f4c447c3655727035399ba85f406
brenimsilva <brenimsilva7@gmail.com>
2023-09-07 14:37:42 -0300

    [Chore] Test

--- src/classes/Logger.ts
+++ src/classes/Logger.ts
@@ -4,35 +4,7 @@ export default class Logger {
     constructor() {}
 
     static buildLog(log: BattleLogType) {
-        switch(log.action) {
-            case Action.DEATH: {
-                this.buildDeath();
-                break;
-            }
-            case Action.DROP: {
-                break;
-            }
-            case Action.POTION: {
-                break;
-            }
-            case Action.SKILL_BUFF: {
-                break;   
-            }
-            case Action.SKILL_OFFENSIVE: {
-                break;
-            }
-            case Action.SLASH: {
-                break;
-            }
-            case Action.STAB: {
-                break;
-            }
-            default: {
-                break;
-            }
-        }
     }
-
     static buildDeath() {
         return {} as BattleLogType;
     }
